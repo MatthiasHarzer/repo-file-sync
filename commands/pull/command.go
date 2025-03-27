e34544fa2a14378916898cb8bf37f5dbcb9b662d
Matthias Harzer <matthias.harzer03@gmail.com>
2025-03-27 16:16:15 +0100

    feat: add pull command to pull from the remote repository without replacing any local files

---
+++ commands/pull/command.go
@@ -0,0 +1,50 @@
+package pull
+
+import (
+	"errors"
+
+	"repo-file-sync/config"
+	"repo-file-sync/database"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var (
+	baseDir string
+)
+
+func init() {
+	Command.Flags().StringVarP(&baseDir, "dir", "d", "", "The directory to search for repositories. Defaults to the current working directory.")
+}
+
+var Command = &cobra.Command{
+	Use:   "pull",
+	Short: "Pulls the latest changes from the remote repositories without replacing any local files",
+	Long:  "Pulls the latest changes from the remote repositories without replacing any local files",
+	RunE: func(c *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			panic(err)
+		}
+
+		if cfg.LocalOnly {
+			return errors.New("pull command is not available in local only mode")
+		}
+
+		db, err := database.NewRepoDatabase(cfg.DatabasePath)
+		if err != nil {
+			return errors.New("could not open database repo\n\ndid you run `repo-file-sync init`?")
+		}
+
+		println("Pulling from", color.GreenString(cfg.DatabaseRepoURL))
+
+		err = db.Pull()
+		if err != nil {
+			panic(err)
+		}
+
+		color.RGB(200, 200, 200).Print("Pulled changes")
+		return nil
+	},
+}
