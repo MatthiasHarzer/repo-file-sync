a6cdf3877ce15922c716e7fffbee73fde631ce80
Matthias Harzer <matthias.harzer03@gmail.com>
2025-04-09 13:01:31 +0200

    state

--- repository/file.go
+++ repository/file.go
@@ -1,6 +1,28 @@
 package repository
 
+import "path/filepath"
+
 type File struct {
 	AbsolutePath     string
 	PathFromRepoRoot string
+}
+
+func splitPath(path string) []string {
+	dir, file := filepath.Split(path)
+	if dir == "" {
+		return []string{file}
+	}
+	return append(splitPath(filepath.Clean(dir)), file)
+}
+
+func fixPath(path string) string {
+	path = filepath.ToSlash(path)
+	path = filepath.Clean(path)
+
+}
+
+func CopyFile(src, dst string) error {
+	srcAdjusted := ""
+	destAdjusted := ""
+
 }
---
+++ repository/replacements.go
@@ -0,0 +1,5 @@
+package repository
+
+var FolderReplacements = map[string]string{
+	".git": "$.git",
+}
